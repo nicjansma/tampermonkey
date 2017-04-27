ff6de471ecdce9cf0f93e1b7b862db281cfce02a
Nic Jansma <nic@nicj.net>
2017-04-26 21:49:52 -0400

    Add auto-updating

--- longtasks.js
+++ longtasks.js
@@ -6,6 +6,8 @@
 // @grant        none
 // @match        */*
 // @run-at       document-start
+// @downloadUrl  https://raw.githubusercontent.com/nicjansma/tampermonkey/master/longtasks.js
+// @updateUrl    https://raw.githubusercontent.com/nicjansma/tampermonkey/master/longtasks.meta.js
 // ==/UserScript==
 
 (function() {
---
+++ longtasks.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         LongTasks
+// @version      1.0
+// @description  Logs LongTasks to the console
+// @author       Nic Jansma (nic@nicj.net)
+// @grant        none
+// @match        */*
+// @run-at       document-start
+// @downloadUrl  https://raw.githubusercontent.com/nicjansma/tampermonkey/master/longtasks.js
+// @updateUrl    https://raw.githubusercontent.com/nicjansma/tampermonkey/master/longtasks.meta.js
+// ==/UserScript==
