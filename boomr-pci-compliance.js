d4633edde7c4ebca4762ef7fa33d5b40a8a4fcd8
Nic Jansma <nic@nicj.net>
2018-04-18 13:49:09 -0400

    boomr-pci-compliance: Fix @match

--- boomr-pci-compliance.js
+++ boomr-pci-compliance.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Boomerang PCI Compliance check
 // @namespace    http://nicj.net
-// @version      1.0
+// @version      1.1
 // @description  Validates that a mPulse is suitable for Boomerang => 1.486.0
 // @author       Nic Jansma (nic@nicj.net)
 // @grant        none
-// @match        *
+// @match        */*
 // @run-at       document-idle
 // @downloadUrl  https://raw.githubusercontent.com/nicjansma/tampermonkey/master/boomr-pci-compliance.js
 // @updateUrl    https://raw.githubusercontent.com/nicjansma/tampermonkey/master/boomr-pci-compliance.meta.js
