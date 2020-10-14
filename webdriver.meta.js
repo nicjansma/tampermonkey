07cb994ac6283433711b57c79c280af7c57e3ea7
Nic Jansma <nic@nicj.net>
2020-10-14 13:04:10 -0400

    Add WebDriver script

---
+++ webdriver.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Fake webdriver
+// @namespace    http://nicj.net
+// @version      1.0
+// @description  Pretends to be webdriver by setting navigator.webdriver
+// @author       Nic Jansma (nic@nicj.net)
+// @grant        none
+// @match        *://*/*
+// @run-at       document-start
+// @downloadUrl  https://raw.githubusercontent.com/nicjansma/tampermonkey/master/webdriver.js
+// @updateUrl    https://raw.githubusercontent.com/nicjansma/tampermonkey/master/webdriver.meta.js
+// ==/UserScript==
